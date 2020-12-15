6c4b8a4d7acedf85ed834e8ebdb469447695e94b
Cristian Mundo <00086917@uca.edu.sv>
2020-12-15 16:43:51 -0600

    estilos log in

--- src/components/Auth/LoginScreen.js
+++ src/components/Auth/LoginScreen.js
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import imagen from "../../assets/img/inicio-sesion.png"
+import logo from "../../assets/img/logo.png"
 export const LoginScreen = () => {
     return (
-        <div>
-            <h1>Login Screeen</h1>
+        <div className="auth__container">
+            <div className="auth__image">
+                <img alt="una imagen" src={imagen} />
+            </div>
+            <div className="auth__right-container">
+                <div className="auth__mini-nav">
+                    <img alt="logo" src={logo}/>
+                    <div style={{
+                        display:'flex', 
+                        alignItems:'center'}}>
+                        <p style={{paddingRight:20}}>Sin cuenta?</p>
+                        <button className="btn-medium">
+                            <a className="link-dec" href="/auth/register">Registrate</a>
+                        </button>
+                    </div>  
+                </div>
+                <h2 className="auth__h2-align">Iniciar sesión</h2>
+                <form className="auth__form">
+                    <input 
+                        type="text" 
+                        placeholder="Correo"
+                        className="auth__input"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Contraseña"
+                        className="auth__input"    
+                    />
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                    >
+                        Iniciar sesion
+                    </button>
+                </form>
+                <button className="btn btn-facebook">
+                    <i style={{marginRight:30}} class="fab fa-facebook-f"></i>
+                    Iniciar sesión con facebook
+                </button>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import {CanvasApp} from "./CanvasApp"
+import "./styles/styles.scss"
 
 ReactDOM.render(
   <CanvasApp/>,
