3e3ac013bbebb79c82ca7b4c39605645705642e8
Cristian Mundo <00086917@uca.edu.sv>
2021-02-01 02:40:57 -0600

    branch created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import {CanvasApp} from "./CanvasApp"
-import "./styles/styles.scss"
+import './styles/base/base.css'
+import './styles/components/auth.css'
+import './styles/components/buttons.css'
+import './styles/components/carousel.css'
+import './styles/components/cart.css'
+import './styles/components/foot.css'
+import './styles/components/home.css'
+import './styles/components/main.css'
+import './styles/components/navbar.css'
+import './styles/components/questions.css'
+import './styles/components/related.css'
+import './styles/components/realated.css'
+import './styles/components/selled.css'
+import './styles/components/temas.css'
+import './styles/components/upload.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
