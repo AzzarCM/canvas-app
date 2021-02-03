1da7c592e5df1ed9105730f027725e64d69ac087
Cristian Mundo <00086917@uca.edu.sv>
2021-02-03 02:00:43 -0600

    webkit to home

--- src/CanvasApp.js
+++ src/CanvasApp.js
@@ -3,7 +3,21 @@ import { AppRouter } from "./routers/AppRouter"
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import 'normalize.css'
-import './styles/main.css'
+import './styles/components/home.css';
+import './styles/components/auth.css';
+import './styles/components/buttons.css';
+import './styles/components/carousel.css';
+import './styles/components/cart.css';
+import './styles/components/foot.css';
+import './styles/components/main.css';
+import './styles/components/navbar.css';
+import './styles/components/questions.css';
+import './styles/components/realated.css';
+import './styles/components/related.css';
+import './styles/components/selled.css';
+import './styles/components/temas.css';
+import './styles/components/upload.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const CanvasApp = () => {
