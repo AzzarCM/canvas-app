1df066b3fc2bb26e1d6b56338bb3e26db4bd6400
Cristian Mundo <00086917@uca.edu.sv>
2021-02-03 01:06:23 -0600

    change another thing 'bout css

--- src/CanvasApp.js
+++ src/CanvasApp.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { AppRouter } from "./routers/AppRouter"
 import { Provider } from 'react-redux'
 import { store } from './store/store'
-
+import 'normalize.css'
+import './styles/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const CanvasApp = () => {
     return (
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import {CanvasApp} from "./CanvasApp"
-import './styles/base/base.css'
-import './styles/components/auth.css'
-import './styles/components/buttons.css'
-import './styles/components/carousel.css'
-import './styles/components/cart.css'
-import './styles/components/foot.css'
-import './styles/components/home.css'
-import './styles/components/main.css'
-import './styles/components/navbar.css'
-import './styles/components/questions.css'
-import './styles/components/related.css'
-import './styles/components/realated.css'
-import './styles/components/selled.css'
-import './styles/components/temas.css'
-import './styles/components/upload.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import {CanvasApp} from "./CanvasApp";
 
 ReactDOM.render(
   <CanvasApp/>,
