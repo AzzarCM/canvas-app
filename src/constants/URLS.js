07eca4b18e8a4c178c4b064d107cb5817b272ad3
Cristian Mundo <00086917@uca.edu.sv>
2021-05-23 21:00:38 -0600

    url changed

--- src/constants/URLS.js
+++ src/constants/URLS.js
@@ -1,4 +1,4 @@
-export const API_HOST = `https://canvas-dev21.herokuapp.com/v1.0.0`;
+export const API_HOST = `https://api-o.canvasframeit.com/v1.0.0`;
 //https://canvas-dev21.herokuapp.com/v1.0.0 this 2021
 //http://canvasframeit.com:3000/api
 //https://api-rest-canvas.herokuapp.com/v1  //prueba
@@ -6,3 +6,6 @@ export const API_HOST = `https://canvas-dev21.herokuapp.com/v1.0.0`;
 
 //https://protected-ocean-09865.herokuapp.com/v1 //funcional abril 2021
 //https://canvas-dev21.herokuapp.com/v1.0.0 //prueba 2.0 ?
+
+
+//https://api-o.canvasframeit.com/v1.0.0 ultima dada
