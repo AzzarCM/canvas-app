d769ff989701eb86e5dbbcc7e0342f9ed27f3d51
Cristian Mundo <00086917@uca.edu.sv>
2021-07-27 20:31:59 -0600

    agregando fecha de entrega

--- src/components/history/HeaderOrderDetails.js
+++ src/components/history/HeaderOrderDetails.js
@@ -6,76 +6,88 @@ import step2 from "../../assets/img/step2.png";
 import step3 from "../../assets/img/step3.png";
 import step4 from "../../assets/img/step4.png";
 
-function HeaderOrderDetails({order}) {
+function HeaderOrderDetails({ order }) {
     return (
         <>
-            {order && 
-            <>
-                <div className="row d-flex justify-content-between px-3 top delivery">
-                    <div className="d-flex header-bar delivery">
-                        <h5>
-                            ORDEN <span className="text-primary font-weight-bold">{`# ${order[0].id}`}</span>
-                        </h5>
+            {order &&
+                <>
+                    <div className="row d-flex justify-content-between px-3 top delivery">
+                        <div className="d-flex header-bar delivery">
+                            <h5>
+                                ORDEN <span className="text-primary font-weight-bold">{`# ${order[0].id}`}</span>
+                            </h5>
+                        </div>
+                        <div className="d-flex flex-column text-sm-right delivery">
+                            {
+                                order[0].delivery_date ?
+                                    <>
+                                        <p className="mb-0">
+                                            Orden entregada
+                                        </p>
+                                        <p style={{ fontWeight: 'bold', marginBottom: 0 }}>{`${moment(order[0].delivery_date).format("DD-MM-YYYY")} `}</p>
+                                    </>
+                                    :
+                                    <>
+                                        <p className="mb-0">
+                                        Fecha de entrega estimada: 
+                                        </p>
+                                        <p style={{fontWeight: 'bold', marginBottom: 0}}>{`${moment(order[0].order_date).add(3,'days').format("DD-MM-YYYY")} al ${moment(order[0].order_date).add(5,'days').format("DD-MM-YYYY")}`}</p>
+                                    </>
+                            }
+                        </div>
                     </div>
-                    <div className="d-flex flex-column text-sm-right delivery">
-                        <p className="mb-0">
-                            Fecha de entrega estimada: 
-                        </p>
-                        <p style={{fontWeight: 'bold', marginBottom: 0}}>{`${moment(order[0].order_date).add(3,'days').format("DD-MM-YYYY")} al ${moment(order[0].order_date).add(5,'days').format("DD-MM-YYYY")}`}</p>
-                    </div>
-                </div>
-                <div className="row d-flex justify-content-center delivery progress-column">
+                    <div className="row d-flex justify-content-center delivery progress-column">
                         <div className="col-12">
                             <ul id="progressbar" className="text-center">
                                 {progressOrderBar(order[0].status)}
                             </ul>
                         </div>
-                </div>
-                <div className="row justify-content-between top container-labels delivery">
-                    <div className="row d-flex icon-content icon-container delivery">
-                        {" "}
-                        <img className="icon_header" src={step1} />
-                        <div className="d-flex flex-column text-container delivery">
-                            <p className="font-weight-bold text-label">
-                            Orden
-                            <br />
-                            Pendiente
-                            </p>
+                    </div>
+                    <div className="row justify-content-between top container-labels delivery">
+                        <div className="row d-flex icon-content icon-container delivery">
+                            {" "}
+                            <img className="icon_header" src={step1} />
+                            <div className="d-flex flex-column text-container delivery">
+                                <p className="font-weight-bold text-label">
+                                    Orden
+                                    <br />
+                                    Pendiente
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row d-flex icon-content icon-container delivery">
+                            {" "}
+                            <img className="icon_header" src={step2} />
+                            <div className="d-flex flex-column text-container delivery">
+                                <p className="font-weight-bold text-label">
+                                    Orden
+                                    <br />
+                                    Confirmada
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row d-flex icon-content icon-container delivery">
+                            {" "}
+                            <img className="icon_header" src={step3} />
+                            <div className="d-flex flex-column text-container delivery">
+                                <p className="font-weight-bold text-label">
+                                    Orden <br />
+                                    En camino
+                                </p>
+                            </div>
+                        </div>
+                        <div className="row d-flex icon-content icon-container delivery">
+                            {" "}
+                            <img className="icon_header" src={step4} />
+                            <div className="d-flex flex-column text-container delivery">
+                                <p className="font-weight-bold text-label">
+                                    Orden <br />
+                                    Entregada
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="row d-flex icon-content icon-container delivery">
-                    {" "}
-                    <img className="icon_header" src={step2} />
-                        <div className="d-flex flex-column text-container delivery">
-                            <p className="font-weight-bold text-label">
-                            Orden
-                            <br />
-                            Confirmada
-                            </p>
-                        </div>
-                    </div>
-                    <div className="row d-flex icon-content icon-container delivery">
-                    {" "}
-                    <img className="icon_header" src={step3} />
-                        <div className="d-flex flex-column text-container delivery">
-                            <p className="font-weight-bold text-label">
-                            Orden <br />
-                            En camino
-                            </p>
-                        </div>
-                    </div>
-                    <div className="row d-flex icon-content icon-container delivery">
-                    {" "}
-                    <img className="icon_header" src={step4} />
-                        <div className="d-flex flex-column text-container delivery">
-                            <p className="font-weight-bold text-label">
-                            Orden <br />
-                            Entregada
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </>
+                </>
             }
         </>
     )
